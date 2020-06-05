25ac33615ecd458ae83321b9ea27f830f2aa3be2
algernon <aeneas@aloku.net>
2020-06-05 11:06:50 +1000

    Prepare for 1.0.2 release.

--- Painter/Mod.cs
+++ Painter/Mod.cs
@@ -8,7 +8,7 @@ namespace Painter
     public class RepaintMod : IUserMod
     {
         public string Name => "Repaint " + Version;
-        public static string Version => "1.0.1";
+        public static string Version => "1.0.2";
         public string Description => Translation.GetTranslation("PAINTER-DESCRIPTION");
         public static Translation Translation = new Translation();
 
